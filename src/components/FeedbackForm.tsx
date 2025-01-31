f773624aa2190887fbbc9a92da3037a53a2e8b6c
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-01-31 12:00:26 -0600

    add state for handling text and derive how many characters are left from that state, add lib direcrtory with constants file

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,16 +1,22 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const charCount = MAX_CHARACTERS - text.length;
   return (
     <form className="form">
       <textarea
         id="feedback-textarea"
         spellCheck={false}
         placeholder="blabla"
+        onChange={(e) => setText(e.target.value)}
       />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
