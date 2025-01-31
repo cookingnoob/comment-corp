f409e02fc79af5e1a5323430a7cdba1d72c51e68
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-01-31 11:57:39 -0600

    add component structure and styles

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <small>
+        <p>
+          &copy; Copyright by{" "}
+          <a target="_blank" href="https://bytegrad.com">
+            Bytegrad.com
+          </a>
+          Intended for your learning porfolio
+        </p>
+        <p>
+          <span className="u-bold u-italic">Not allowed</span> to use as your
+          own teaching material.
+        </p>
+      </small>
+    </footer>
+  );
+}
