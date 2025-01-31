ded044e68b6e5a81d061dce02e6c840b907281d3
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-01-31 12:21:21 -0600

    separate change event to separate function

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -4,13 +4,22 @@ import { MAX_CHARACTERS } from "../lib/constants";
 export default function FeedbackForm() {
   const [text, setText] = useState("");
   const charCount = MAX_CHARACTERS - text.length;
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    if (newText.length > MAX_CHARACTERS) {
+      return;
+    }
+    setText(newText);
+  };
+
   return (
     <form className="form">
       <textarea
         id="feedback-textarea"
         spellCheck={false}
         placeholder="blabla"
-        onChange={(e) => setText(e.target.value)}
+        onChange={handleChange}
       />
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remember to #hashtag the company
