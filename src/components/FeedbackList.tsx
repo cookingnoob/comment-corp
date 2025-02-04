4d45fde6a1afb943c13c76e49aeab317d84fbd15
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-02-04 13:33:34 -0600

    add addItemToList function to add to current state

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -2,11 +2,27 @@ import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
 import ErrorMessage from "./ErrorMessage";
+import { TFeedbackItem } from "../lib/types";
 
 export default function FeedbackList() {
-  const [feedBackList, setFeedbackList] = useState([]);
+  const [feedBackList, setFeedbackList] = useState<TFeedbackItem[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
+  const addItemToList = (text: string) => {
+    const companyName = text
+      .split(" ")
+      .find((w) => w.includes("#"))!
+      .substring(1);
+    const newItem: TFeedbackItem = {
+      upvoteCount: 0,
+      daysAgo: 0,
+      text: text,
+      companyName: companyName,
+      badgeLetter: companyName.substring(0, 1).toLocaleUpperCase(),
+    };
+    setFeedbackList([...feedBackList, newItem]);
+  };
 
   useEffect(() => {
     setIsLoading(true);
