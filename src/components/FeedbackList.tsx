e461206361710d9f638bbad265a456d5ca940d01
cookingnoob <110266985+cookingnoob@users.noreply.github.com>
2025-02-04 12:32:21 -0600

    add error message state

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,17 +1,38 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedBackList, setFeedbackList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+
   useEffect(() => {
+    setIsLoading(true);
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
-      .then((response) => response.json())
-      .then((data) => setFeedbackList(data.feedbacks));
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error();
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setFeedbackList(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setErrorMessage("Something went wrong");
+        setIsLoading(false);
+      });
   }, []);
+
   return (
     <ol className="feedback-list">
+      {isLoading ? <Spinner /> : null}
+      {errorMessage ? <ErrorMessage message={errorMessage} /> : null}
       {feedBackList.map((item) => (
         <FeedbackItem key={item.id} feedBackItem={item} />
       ))}
